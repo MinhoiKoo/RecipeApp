859b0e600c5702eda5c36f9c6183dbedaa7aa94e
MinhoiKoo <minlove2013@gmail.com>
2023-07-03 19:36:20 +0900

    [Feat] 레시피 검색 구현

--- app/src/main/java/com/minhoi/recipeapp/SearchFragment.kt
+++ app/src/main/java/com/minhoi/recipeapp/SearchFragment.kt
@@ -1,18 +1,24 @@
 package com.minhoi.recipeapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.minhoi.recipeapp.databinding.FragmentSearchBinding
+import com.minhoi.recipeapp.model.RecipeDto
 
 class SearchFragment : Fragment() {
 
     // 메뉴명, 재료명 으로 검색 가능하도록 구현 예정
     private lateinit var binding : FragmentSearchBinding
+    private lateinit var viewModel : SearchViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,11 +29,33 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_search, container, false )
 
+        binding.inputRecipe.setOnEditorActionListener { v, actionId, event ->
+            var handled = false
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                // 수정 필요. EditText <-> liveData 오류로 인한 임시 코드
+                viewModel._mutableSearchInput.value = binding.inputRecipe.text.toString()
+                viewModel.searchRcp()
+                handled = true
+                Log.d("edit",viewModel._mutableSearchInput.value.toString())
+            }
+            handled
+        }
+
+        val rv = binding.searchRv
+
+        viewModel.searchList.observe(viewLifecycleOwner) {
+            rv.adapter = RcpListAdapter(context!!, it as ArrayList<RecipeDto>)
+            rv.layoutManager = GridLayoutManager(activity, 2)
+        }
         binding.homeBtn.setOnClickListener {
             it.findNavController().navigate(R.id.action_searchFragment_to_homeFragment)
         }
+
+
         return binding.root
     }
 }
