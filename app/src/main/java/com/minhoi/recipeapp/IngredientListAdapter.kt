02aede6fab0ac1b2d609392158c9b46be549fdf7
MinhoiKoo <minlove2013@gmail.com>
2023-07-18 15:50:21 +0900

    [Feat] 재료 리스트 실시간으로 반영할 RecyclerView, adapter 생성

---
+++ app/src/main/java/com/minhoi/recipeapp/IngredientListAdapter.kt
@@ -0,0 +1,51 @@
+package com.minhoi.recipeapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class IngredientListAdapter(private val onDeleteClickListener: OnDeleteClickListener) :
+    RecyclerView.Adapter<IngredientListAdapter.IngredientViewHolder>() {
+
+    private val ingredientList = mutableListOf<String>()
+
+    fun setIngredients(ingredients: List<String>) {
+        ingredientList.clear()
+        ingredientList.addAll(ingredients)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.ingredient_item, parent, false)
+        return IngredientViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
+        val ingredient = ingredientList[position]
+        holder.bind(ingredient)
+    }
+
+    override fun getItemCount(): Int {
+        return ingredientList.size
+    }
+
+    inner class IngredientViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textViewIngredient: TextView = itemView.findViewById(R.id.textViewIngredient)
+        private val imageViewDelete: ImageView = itemView.findViewById(R.id.imageViewDelete)
+
+        fun bind(ingredient: String) {
+            textViewIngredient.text = ingredient
+            imageViewDelete.setOnClickListener {
+                onDeleteClickListener.onDeleteClick(adapterPosition)
+            }
+        }
+    }
+
+    interface OnDeleteClickListener {
+        fun onDeleteClick(position: Int)
+    }
+}
