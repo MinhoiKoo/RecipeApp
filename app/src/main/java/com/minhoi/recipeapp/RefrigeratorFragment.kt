101599128462478206c0df2ec42122e6f32ef1a8
MinhoiKoo <minlove2013@gmail.com>
2023-07-18 15:19:33 +0900

    [Feat] 재료명 입력 실시간으로 공백 기준으로 리스트에 추가

--- app/src/main/java/com/minhoi/recipeapp/RefrigeratorFragment.kt
+++ app/src/main/java/com/minhoi/recipeapp/RefrigeratorFragment.kt
@@ -1,23 +1,32 @@
 package com.minhoi.recipeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.minhoi.recipeapp.api.Ref
+import com.minhoi.recipeapp.databinding.FragmentRefrigeratorBinding
 import com.minhoi.recipeapp.model.RecipeDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class RefrigeratorFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val TAG = RefrigeratorFragment::class.java.simpleName
 
+    private lateinit var binding : FragmentRefrigeratorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,30 +36,100 @@ class RefrigeratorFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        var filterList = arrayListOf<RecipeDto>()
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_refrigerator, container, false )
 
-        Ref.recipeDataRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                for (data in snapshot.children) {
-                    val value = data.getValue(RecipeDto::class.java)
 
-                    if (value != null) {
-                        if(value.rcp_PARTS_DTLS?.contains("새우") == true && value.rcp_PARTS_DTLS != "건새우") {
-                            filterList.add(value)
-                            Log.d("filter", value.toString())
-                        }
-                    }
-                }
+        binding.homeBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_refrigeratorFragment_to_homeFragment)
+        }
+
+        binding.searchBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_refrigeratorFragment_to_searchFragment)
+        }
+
+        binding.mypageBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_refrigeratorFragment_to_mypageFragment)
+        }
+
+        val input = binding.inputIngredient
+        val array = mutableListOf<String>()
+
+        input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // 처음 입력이 공백으로 들어오면 EditText clear / 사용자 입력 공백 기준으로 List에 추가.
+                Log.d(TAG, s.toString())
+                if(s != null && s.toString().contains(" ")){
+                    val ingredient = s.toString().trim()
+
+                    if(ingredient == "") {
+                        input.text.clear()
+                    } else {
+                        array.add(ingredient)
+                        input.text.clear()
+                    }
+
+                    Log.d("array", array.toString())
+                }
             }
 
         })
 
-        return inflater.inflate(R.layout.fragment_refrigerator, container, false)
+
+        binding.searchRefriBtn.setOnClickListener {
+
+            getRecipe(array) {
+                val intent = Intent(getActivity(), RecipeListActivity::class.java)
+                intent.putExtra("recipeList", it)
+                startActivity(intent)
+            }
+
+        }
+
+        return binding.root
+    }
+
+
+    // 레시피 전체 정보를 받아와서 레시피의 재료가 사용자가 선택한 재료를 필터링하여 리스트에 추가.
+
+    private fun getRecipe(ingredients: List<String>, callback: (ArrayList<String>) -> Unit) {
+        val filterKeyList = arrayListOf<String>()
+        var remainingIngredients = ingredients.size
+
+        ingredients.forEach { ingredient ->
+            Ref.recipeDataRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (data in dataSnapshot.children) {
+                        val value = data.getValue(RecipeDto::class.java)
+                        val key = data.key.toString()
+                        if (value != null && value.rcp_PARTS_DTLS?.contains(ingredient) == true) {
+                            filterKeyList.add(key)
+                            Log.d("filter", key)
+                        }
+                    }
+                    remainingIngredients--
+                    if (remainingIngredients == 0) {
+                        // All ingredients have been processed, invoke the callback here.
+                        callback(filterKeyList)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    remainingIngredients--
+                    if (remainingIngredients == 0) {
+                        // All ingredients have been processed, invoke the callback here.
+                        callback(filterKeyList)
+                    }
+                }
+            })
+        }
     }
 
 }
