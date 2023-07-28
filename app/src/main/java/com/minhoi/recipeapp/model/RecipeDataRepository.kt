970b915df22bc3e3c8031df58924beed04faae3c
MinhoiKoo <minlove2013@gmail.com>
2023-07-28 19:14:20 +0900

    [Feat] 레시피 정보 가져오는 Repository 생성

---
+++ app/src/main/java/com/minhoi/recipeapp/model/RecipeDataRepository.kt
@@ -0,0 +1,32 @@
+package com.minhoi.recipeapp.model
+
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.minhoi.recipeapp.api.Ref
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class RecipeDataRepository {
+
+    suspend fun getRecipeInfo(rcpSeq : String) : RecipeDto{
+
+        return suspendCoroutine<RecipeDto> { continuation ->
+            val postListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val data = dataSnapshot.getValue(RecipeDto::class.java)
+                    if(data != null) {
+                        continuation.resume(data)
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Getting Post failed, log a message
+                    continuation.resumeWithException(databaseError.toException())
+                }
+            }
+            Ref.myRecipeDataRef.child(rcpSeq).addListenerForSingleValueEvent(postListener)
+        }
+    }
+}
