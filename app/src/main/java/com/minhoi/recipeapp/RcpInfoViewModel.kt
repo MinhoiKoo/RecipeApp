6e23bd05ff3ccfd3ed88703b5e878d77cd1a93d8
MinhoiKoo <128913867+MinhoiKoo@users.noreply.github.com>
2023-08-13 23:21:22 +0900

    [Feat] 북마크 Count 구현

--- app/src/main/java/com/minhoi/recipeapp/RcpInfoViewModel.kt
+++ app/src/main/java/com/minhoi/recipeapp/RcpInfoViewModel.kt
@@ -1,5 +1,6 @@
 package com.minhoi.recipeapp
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -16,6 +17,7 @@ import kotlinx.coroutines.withContext
 
 class RcpInfoViewModel : ViewModel() {
 
+    private val TAG = RcpInfoViewModel::class.simpleName
     private val kakaoUserRepository = KakaoUserRepository()
     private val recipeDataRepository = RecipeDataRepository()
     private var ref = Ref()
@@ -34,27 +36,56 @@ class RcpInfoViewModel : ViewModel() {
         }
     }
 
+    // 사용자가 레시피 정보 액티비티에 들어올 때 북마크 한 레시피인지 아닌지 검사
     fun isBookmark (userId : String, rcpSeq : String) {
 
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // 북마크 하지 않은 레시피이면 DB에 등록 후 색칠된 버튼
-                _isBookmarked.value = dataSnapshot.exists()
-            }
+        when(userId) {
+            "" -> _isBookmarked.value = false
+            else -> {
+                val postListener = object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        // 북마크 하지 않은 레시피이면 DB에 등록 후 색칠된 버튼
+                        _isBookmarked.value = dataSnapshot.exists()
+                    }
 
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Getting Post failed, log a message
+                    }
+                }
+                Ref.userRef.child(userId).child("bookmarkedRecipe").child(rcpSeq!!).addValueEventListener(postListener)
             }
         }
-        Ref.userRef.child(userId).child("bookmarkedRecipe").child(rcpSeq!!).addValueEventListener(postListener)
     }
 
     fun setBookmark(userId : String, rcpSeq: String) {
         Ref.userRef.child(userId).child("bookmarkedRecipe").child(rcpSeq).child("whenBookmarked").setValue(ref.getDate())
+        updateBookmarkCount(rcpSeq, true)
+        Log.d(TAG, "setBookmark: $rcpSeq")
     }
 
     fun deleteBookmark(userId : String, rcpSeq: String) {
         Ref.userRef.child(userId).child("bookmarkedRecipe").child(rcpSeq).removeValue()
+        updateBookmarkCount(rcpSeq, false)
+        Log.d(TAG, "deleteBookmark: $rcpSeq")
+
+    }
+
+    // 사용자가 북마크 추가/제거에 따라 레시피의 bookmarkCount +-1
+    private fun updateBookmarkCount(rcpSeq : String, upDown : Boolean) {
+        val ref = Ref.myRecipeDataRef
+        ref.child(rcpSeq).child("bookmarkCount")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val currentCount = snapshot.getValue(Int::class.java)?:0
+                    when(upDown) {
+                        true -> ref.child(rcpSeq).child("bookmarkCount").setValue(currentCount + 1)
+                        false -> ref.child(rcpSeq).child("bookmarkCount").setValue(currentCount - 1)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
     }
 
 }
