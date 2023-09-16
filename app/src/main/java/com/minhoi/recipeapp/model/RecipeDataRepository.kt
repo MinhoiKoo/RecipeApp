adbec194f3b841ff73f78501dc307c66cd7fe9b5
MinhoiKoo <128913867+MinhoiKoo@users.noreply.github.com>
2023-09-17 03:10:26 +0900

    [Refactor] DB에서 레시피 정보 가져오는 로직 Repository로 분리

--- app/src/main/java/com/minhoi/recipeapp/model/RecipeDataRepository.kt
+++ app/src/main/java/com/minhoi/recipeapp/model/RecipeDataRepository.kt
@@ -1,5 +1,6 @@
 package com.minhoi.recipeapp.model
 
+import android.util.Log
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -27,6 +28,59 @@ class RecipeDataRepository {
                 }
             }
             Ref.myRecipeDataRef.child(rcpSeq).addListenerForSingleValueEvent(postListener)
+        }
+    }
+
+    suspend fun getRandomRcp() : ArrayList<RecipeDto> {
+
+        return suspendCoroutine {
+            var count = 0
+            val postListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // Get Post object and use the values to update the UI
+                    val recipeList = arrayListOf<RecipeDto>()
+                    for (postSnapshot in dataSnapshot.children) {
+                        if(count ==6) {break}
+                        val recipeData = postSnapshot.getValue(RecipeDto::class.java)
+                        Log.d("recipeData",recipeData.toString())
+
+                        if (recipeData != null) {
+                            recipeList.add(recipeData)
+                            count++
+                        }
+                    }
+                    it.resume(recipeList)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+//                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                }
+            }
+            Ref.recipeDataRef.addListenerForSingleValueEvent(postListener)
+        }
+
+
+    }
+
+    suspend fun getPopularRcp() : ArrayList<RecipeDataModel> {
+        return suspendCoroutine {
+            val queue = arrayListOf<RecipeDataModel>()
+            val postListener = object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for(data in snapshot.children) {
+                        val recipeData = data.getValue(RecipeDataModel::class.java)
+                        if(recipeData != null) {
+                            queue.add(recipeData)
+                        }
+                    }
+                    it.resume(queue)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+            }
+            Ref.myRecipeDataRef.orderByChild("bookmarkCount").addListenerForSingleValueEvent(postListener)
         }
     }
 }
--- app/src/main/java/com/minhoi/recipeapp/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/minhoi/recipeapp/ui/viewmodel/HomeViewModel.kt
@@ -1,88 +1,59 @@
 package com.minhoi.recipeapp.ui.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
+import androidx.lifecycle.viewModelScope
 import com.minhoi.recipeapp.api.MyApi
-import com.minhoi.recipeapp.api.Ref
 import com.minhoi.recipeapp.api.RetrofitInstance
 import com.minhoi.recipeapp.model.KakaoUserRepository
 import com.minhoi.recipeapp.model.RecipeDataModel
+import com.minhoi.recipeapp.model.RecipeDataRepository
 import com.minhoi.recipeapp.model.RecipeDto
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeViewModel : ViewModel() {
     private val retrofitInstance : MyApi = RetrofitInstance.getInstance().create(MyApi::class.java)
     private val kakaoUserRepository = KakaoUserRepository()
+    private val recipeDataRepository = RecipeDataRepository()
 
     private var _mutableRcpList = MutableLiveData<List<RecipeDto>>()
     val liveRcpList : LiveData<List<RecipeDto>>
         get() = _mutableRcpList
+
+    private var _mutablePopularList = MutableLiveData<ArrayList<RecipeDataModel>>()
+    val livePopularList : LiveData<ArrayList<RecipeDataModel>>
+        get() = _mutablePopularList
 
     suspend fun getUser() : String {
         return kakaoUserRepository.getUser()
     }
 
     init {
-        getRandomRcp()
-    }
+        viewModelScope.launch(Dispatchers.IO) {
+            val randomList = setRandomRecipe()
+            val popularList = setPopularRecipe()
+            withContext(Dispatchers.Main) {
+                _mutableRcpList.value = randomList
+                _mutablePopularList.value = popularList
 
-    fun getRandomRcp() {
-
-        var count = 0
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get Post object and use the values to update the UI
-                val recipeList = mutableListOf<RecipeDto>()
-                for (postSnapshot in dataSnapshot.children) {
-                    if(count ==6) {break}
-                    val recipeData = postSnapshot.getValue(RecipeDto::class.java)
-                    Log.d("recipeData",recipeData.toString())
-
-                    if (recipeData != null) {
-                        recipeList.add(recipeData)
-                        count++
-                    }
-                }
-                _mutableRcpList.value = recipeList
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-//                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
             }
         }
-        Ref.recipeDataRef.addListenerForSingleValueEvent(postListener)
-
     }
 
-    // bookmarkCount 순으로 오름차순 정렬하여 deque에 삽입 후 반환
-    suspend fun getPopularRcp() : ArrayList<RecipeDataModel> {
-        return suspendCoroutine {
-            val queue = arrayListOf<RecipeDataModel>()
-            val postListener = object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    for(data in snapshot.children) {
-                        val recipeData = data.getValue(RecipeDataModel::class.java)
-                        if(recipeData != null) {
-                            queue.add(recipeData)
-                        }
-                    }
-                    it.resume(queue)
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                }
-
-            }
-            Ref.myRecipeDataRef.orderByChild("bookmarkCount").addListenerForSingleValueEvent(postListener)
-        }
+    private suspend fun setRandomRecipe() : ArrayList<RecipeDto> {
+        return recipeDataRepository.getRandomRcp()
     }
+
+    private suspend fun setPopularRecipe() : ArrayList<RecipeDataModel> {
+        return recipeDataRepository.getPopularRcp()
+    }
+
+
+
+
 
 
 }
