e2805460d70701b2495849caa193b5f64729212a
MinhoiKoo <128913867+MinhoiKoo@users.noreply.github.com>
2023-09-24 22:16:36 +0900

    [Feat] SwipeRefreshLayout 구현

--- app/src/main/java/com/minhoi/recipeapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/minhoi/recipeapp/ui/fragment/HomeFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -17,13 +16,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.minhoi.recipeapp.ui.viewmodel.HomeViewModel
 import com.minhoi.recipeapp.R
 import com.minhoi.recipeapp.RcpInfoActivity
-import com.minhoi.recipeapp.UserRecipeAddActivity
-import com.minhoi.recipeapp.adapter.viewpager2.recyclerview.PopularRecipeListAdapter
-import com.minhoi.recipeapp.adapter.viewpager2.recyclerview.RecipeListAdapter
+import com.minhoi.recipeapp.adapter.recyclerview.PopularListAdapterDecoration
+import com.minhoi.recipeapp.adapter.recyclerview.PopularRecipeListAdapter
+import com.minhoi.recipeapp.adapter.recyclerview.RecipeListAdapter
+import com.minhoi.recipeapp.adapter.recyclerview.RandomListAdapterDecoration
 import com.minhoi.recipeapp.databinding.FragmentHomeBinding
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment() {
 
@@ -55,61 +53,66 @@ class HomeFragment : Fragment() {
 
 
         // 나만의 레시피 추가 화면으로 이동, userId 는 비동기 방식으로 가져오기 때문에 코루틴 내부에서 순차적으로 실행.
-        lifecycleScope.launch {
-            val userId = viewModel.getUser() // 코루틴 일시중지
-            // resume 후 아래 코드 실행
-            binding.btnAddUserRecipe.setOnClickListener {
-                if(userId == "") {
-                    Toast.makeText(requireContext(), "로그인 후 이용 가능합니다.", Toast.LENGTH_LONG).show()
-                } else {
-                    startActivity(Intent(requireActivity(), UserRecipeAddActivity::class.java))
-                }
-            }
-        }
 
         randomListAdapter = RecipeListAdapter(requireContext()) {
             // onClickListener
-            val intent = Intent(requireActivity(), RcpInfoActivity::class.java)
-            intent.putExtra("rcpSeq", it.rcp_SEQ)
-            startActivity(intent)
+            clickRecipe(it.rcp_SEQ)
+            Log.d(TAG, "onCreateView: ${it.rcp_SEQ}")
         }
 
-        popularListAdapter = PopularRecipeListAdapter(requireContext())
+        popularListAdapter = PopularRecipeListAdapter(requireContext()) {
+            //onClickListener
+            clickRecipe(it)
+            Log.d(TAG, "onCreateView: ${it}")
 
-
+        }
 
         binding.recipeRv.apply {
-            isNestedScrollingEnabled = false
+            setHasFixedSize(true)
             adapter = randomListAdapter
             layoutManager = GridLayoutManager(requireContext(),2)
+            addItemDecoration(RandomListAdapterDecoration())
             }
 
         binding.popularRecipeRv.apply {
             setHasFixedSize(true)
             adapter = popularListAdapter
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            addItemDecoration(PopularListAdapterDecoration())
         }
 
-
-        viewModel.liveRcpList.observe(viewLifecycleOwner) {
-            randomListAdapter.setLists(it)
-        }
-
-        lifecycleScope.launch(Dispatchers.IO) {
-            val queue = viewModel.getPopularRcp()
-            withContext(Dispatchers.Main) {
-                popularListAdapter.setList(queue)
-            }
-        }
+        setObserve()
+        setSwipe()
 
         return binding.root
     }
 
-    private val customSpanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-        override fun getSpanSize(position: Int): Int {
-            return when (concatAdapter.getItemViewType(position)) {
-                R.layout.recipe_random_item_row -> 1
-                else -> 2
+    private fun setObserve() {
+        viewModel.liveRcpList.observe(viewLifecycleOwner) {
+            randomListAdapter.setLists(it)
+        }
+
+        viewModel.livePopularList.observe(viewLifecycleOwner) {
+            popularListAdapter.setList(it)
+        }
+    }
+
+    private fun clickRecipe(rcpSeq : String) {
+        val intent = Intent(requireActivity(), RcpInfoActivity::class.java)
+        intent.putExtra("rcpSeq", rcpSeq)
+        startActivity(intent)
+    }
+
+    private fun setSwipe() {
+        binding.swipeLayout.apply {
+            setColorSchemeResources(R.color.myGreen)
+            setOnRefreshListener {
+                isRefreshing = true
+                lifecycleScope.launch {
+                    viewModel.setRandomRecipe()
+                    // suspend 함수를 호출 -> 데이터를 다 가져온 후(코루틴 일시정지됨) endSwipe() 호출됨
+                    isRefreshing = false
+                }
             }
         }
     }
