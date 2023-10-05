4b96109316d865e98801b89fb6343ac936a21ca6
MinhoiKoo <minlove2013@gmail.com>
2023-10-05 22:57:56 +0900

    [Refactor] 객체 리스트 Intent로 넘겨주기 위해 Percelize 선언

--- app/src/main/java/com/minhoi/recipeapp/model/SelectedIngredientDto.kt
+++ app/src/main/java/com/minhoi/recipeapp/model/SelectedIngredientDto.kt
@@ -1,8 +1,11 @@
 package com.minhoi.recipeapp.model
 
+import android.os.Parcelable
 import com.minhoi.recipeapp.adapter.recyclerview.IngredientListItem
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class SelectedIngredientDto (
-    override val name: String = " ",
-    override val imagePath: String= " ")
-: IngredientListItem
+    override val name: String = "",
+    override val imagePath: String= "")
+: IngredientListItem, Parcelable
